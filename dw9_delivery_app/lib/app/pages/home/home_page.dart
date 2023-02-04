6dad2a9903f32f8160ed100092af58226799ad2e
Camila Sales <camilas.dev@gmail.com>
2023-02-03 23:17:08 -0400

    chore: Loader and messages helpers added

--- dw9_delivery_app/lib/app/pages/home/home_page.dart
+++ dw9_delivery_app/lib/app/pages/home/home_page.dart
@@ -1,11 +1,18 @@
+import 'package:dw9_delivery_app/app/core/ui/helpers/loader.dart';
+import 'package:dw9_delivery_app/app/core/ui/helpers/messages.dart';
 import 'package:dw9_delivery_app/app/core/ui/widgets/delivery_appbar.dart';
 import 'package:dw9_delivery_app/app/models/product_model.dart';
 import 'package:dw9_delivery_app/app/pages/home/widgets/delivery_product_tile.dart';
 import 'package:flutter/material.dart';
 
-class HomePage extends StatelessWidget {
+class HomePage extends StatefulWidget {
   const HomePage({super.key});
 
+  @override
+  State<HomePage> createState() => _HomePageState();
+}
+
+class _HomePageState extends State<HomePage> with Loader, Messages {
   @override
   Widget build(BuildContext context) {
     return Scaffold(
